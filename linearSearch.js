c5b518ed56a81359720a77dcecc1dd2218f1793e
mjimenez3 <mjimenez3@liberty.edu>
2020-05-07 16:12:28 -0500

    new files

---
+++ linearSearch.js
@@ -0,0 +1,8 @@
+const list = ['b', 'a', 'c', 'a', 'd', 'a'];
+
+function search(letter, list) {
+    for (let i = 0; i < list.length; i++) {
+        findIndex.list[i](letter);
+    }return;
+}
+search('a');
---
+++ roman.js
@@ -0,0 +1,12 @@
+let I = 1;
+let V = 5;
+let X = 10;
+let L = 50;
+let C = 100;
+let D = 500;
+let M = 1000;
+
+function toRoman(num) {
+    
+}
+toRoman(12);
