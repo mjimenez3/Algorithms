e229045c9b408fa9e5d2d8a6e36fc5da3572a8c9
mjimenez3 <mjimenez3@liberty.edu>
2020-05-06 20:23:51 -0500

    more assignments

--- csDay1.js
+++ csDay1.js
@@ -1,3 +1,5 @@
+//VET WHO CODE
+
 /*function addNumbers(firstNumber, secondNumber, thirdNumber) {
     while ((firstNumber + secondNumber) <= thirdNumber) {
         (firstNumber + secondNumber);
@@ -6,8 +8,8 @@
 }
 addNumbers(1, 2, 25);*/
 
-
-function first() {
+// Call Stack Example
+/*function first() {
     second(5);
     console.log("First!");
 }
@@ -18,4 +20,30 @@ function second(num) {
 first();
 setTimeout(function() {
     console.log("third");}, 0);
-second(5);
+second(5);*/
+
+//Call Stack Example
+/*function AStack() {
+    BStack(10);
+    console.log("this is A Stack");
+}
+function BStack(k) {
+    console.log(k + k);
+    console.log("this is b stack");
+}
+AStack();
+console.log("this is C Stack");
+BStack(9);*/
+
+
+//Recursive Example
+/*function TimeToLaunch(num) {
+    if (num <= 0) {
+        console.log("Blast Off!");
+        return;
+    }
+    console.log(num);
+    num--;
+    TimeToLaunch(num);
+}
+TimeToLaunch(10);*/
