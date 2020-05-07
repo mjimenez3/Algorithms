7ba180bd29261001c3f8922bdaeb85db8832ae7c
mjimenez3 <mjimenez3@liberty.edu>
2020-05-06 22:53:21 -0500

    new files

---
+++ 99_bottles.js
@@ -0,0 +1,9 @@
+function bottles() {
+    for (let num = 99; num >= 0; num++) {
+      
+        while (num >= 1) {
+            console.log(num + " bottles of beer on the wall, " + num + " bottles of beer, you take one down and pass it around, " + num + " bottles of beer on the wall!");
+            num--;
+        } return}
+    }
+bottles();
