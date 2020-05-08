7ba495aca08e5c8ccf3437553787049287642498
mjimenez3 <mjimenez3@liberty.edu>
2020-05-07 20:07:40 -0500

    added recursive

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,4 +1,4 @@
-function bottles() {
+/*function bottles() {
     for (let num = 99; num >= 0; num++) {
       
         while (num >= 1) {
@@ -6,4 +6,27 @@ function bottles() {
             num--;
         } return}
     }
-bottles();
+bottles();*/
+
+function bottles(n) {
+    if (n <= 0) {
+        console.log("Go to sleep, You're Drunk!");
+        return;
+    } 
+    console.log(n + " bottles of beer on the wall, " + n + " bottles of beer, you take one down and pass it around, " + n + " bottles of beer on the wall!");
+
+    n--;
+    bottles(n);
+}
+bottles(10);
+
+/*function TimeToLaunch(num) {
+    if (num <= 0) {
+        console.log("Blast Off!");
+        return;
+    }
+    console.log(num);
+    num--;
+    TimeToLaunch(num);
+}
+TimeToLaunch(10);*/
