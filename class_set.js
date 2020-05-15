dc7e0a766983738028ef59cbd8e8e8d38b9a9bbe
mjimenez3 <mjimenez3@liberty.edu>
2020-05-15 17:26:59 -0500

    Adding set file

---
+++ class_set.js
@@ -0,0 +1,29 @@
+let mikeSet = new Set();
+
+mikeSet.add('dodgers');
+mikeSet.size
+mikeSet.add(25);
+
+console.log(mikeSet);
+
+mikeSet.size
+mikeSet.delete(25);
+mikeSet.size
+
+mikeSet.has('Dodgers');
+mikeSet.has('dodgers');
+
+let team = {
+    1: 'Steelers',
+    2: 'Lakers',
+};
+
+
+mikeSet.add(team);
+mikeSet.size
+
+mikeSet.add(3);
+mikeSet.has(Math.sqrt(9));
+
+
+
